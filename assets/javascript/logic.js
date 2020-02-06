9e92d2052b8fc7ea384e16954eebd3ef562a6ee5
Zachary <zpyingling9559@gmail.com>
2020-02-06 15:59:38 -0500

    making the chatbox work fully aswell as being able to connect to the database; need to code the functionality of the game when you are connected

---
+++ assets/javascript/logic.js
@@ -0,0 +1,107 @@
+// Make the chat box work, make the connections in the database work
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBaJtL9JYA8EoPjNJ-G_bmrA2r6mGsmQNg",
+  authDomain: "multiplayer-rps-7e427.firebaseapp.com",
+  databaseURL: "https://multiplayer-rps-7e427.firebaseio.com",
+  projectId: "multiplayer-rps-7e427",
+  storageBucket: "multiplayer-rps-7e427.appspot.com",
+  messagingSenderId: "39367904820",
+  appId: "1:39367904820:web:49dad8636e423cac5c4461"
+};
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
+
+// true is flag for player is connected
+let connectedFlag = false;
+// false is flag that no more players are needed
+let needPlayers = true;
+let messages = [];
+let name = "";
+let connectedRef = database.ref(".info/connected");
+let connectionsRef = database.ref("/connections");
+let messagesRef = database.ref("/messages");
+
+// Add connected to firebase
+function addConnected(name) {
+  connectedRef.on("value", function(snap) {
+    // Test and see if this feature is working
+    if (snap.child("connections").numChildren() >= 2) {
+      needPlayers = false;
+    }
+
+    // If they are connected and put a name in the start bar
+    if (snap.val() && name != "") {
+      var con = connectionsRef.push({
+        connected: true,
+        name: name,
+        player: needPlayers,
+        wins: 0,
+        loses: 0
+      });
+      connectedFlag = true;
+      con.onDisconnect().remove();
+    }
+  });
+}
+
+//  Display connected in text area
+function displayConnected(name) {
+  messages.push(name + " has connected.");
+  messagesRef.push(name + " has connected.");
+}
+
+// Scrolls to the bottom of the text area
+function scrollToBottom() {
+  var textarea = document.getElementById("chat-box");
+  textarea.scrollTop = textarea.scrollHeight;
+}
+
+// Puts all of the messages in messages array to the text area
+function displayMessages() {
+  $("#chat-box").empty();
+  for (let i = 0; i < messages.length; i++) {
+    $("#chat-box").append(messages[i] + "&#13;&#10;");
+  }
+  scrollToBottom();
+}
+
+$(document).ready(function() {
+  // Sets local array messages to what messages are saved in the database
+  messagesRef.once("value", function(data) {
+    let tempMessagesValue = Object.values(data.val());
+    for (let i = 0; i < tempMessagesValue.length; i++) {
+      messages[i] = tempMessagesValue[i];
+    }
+    displayMessages();
+  });
+
+  $("#start-button").on("click", function() {
+    if ($("#name-input").val() === "" || connectedFlag === true) {
+      return;
+    } else {
+      name = $("#name-input")
+        .val()
+        .trim();
+      addConnected(name);
+      displayConnected(name);
+      displayMessages();
+    }
+  });
+
+  $("#message-send").on("click", function() {
+    if (connectedFlag === false) {
+      alert(
+        "Please put your name in the top and connect to the game and type in chat."
+      );
+    } else {
+      let tempMessage = $("#message")
+        .val()
+        .trim();
+      $("#message").val("");
+      messages.push(name + ": " + tempMessage);
+      messagesRef.push(name + ": " + tempMessage);
+      displayMessages();
+    }
+  });
+});
